57c9f1073a54abbc04e2b54a1b74046e524ea79e
devdoubleh2 <dev.ismoiljon@gmail.com>
2026-05-15 18:29:26 +0500

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b662d8ohkbg"
+    "revision": "0.d5sa9vcvis8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
